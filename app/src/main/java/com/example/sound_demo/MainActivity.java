4c082f8437523882448aa0014eed2818c334ffd9
Marko Vukosav <marko.vukosav85@gmail.com>
2020-12-27 23:54:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sound_demo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sound_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaParser;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    public void Play (View view){
+    mediaPlayer.start();
+
+    }
+public void pause(View view){
+
+mediaPlayer.pause();
+
+}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.mark);
+
+
+    }
+}
